aa4afd3991a40df39d85a8dae63f5dd5132568f0
icelabex <entg.1216lbex@gmail.com>
2021-01-18 00:50:49 +0800

    Fixed
    deleted repositry accidentaly, make new one which i already completed file.

---
+++ VR_End_107051371/Assets/Script/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+   public void RestartGame()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ VR_End_107051371/Assets/Script/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager :MonoBehaviour
+{
+    [Header("ScoreUI")]
+    public Text textScore;
+    [Header("Reputation")]
+    public int score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Trash")
+        {
+            AddScore();
+            // ifで物が入った時に下記クラスの加点実行
+        }
+    }
+
+    //点数の追加　下記のクォーツはヘッダーの呼び出し
+    private void AddScore()
+    {
+        score += 5;
+        //なぜかscoreだけをtextとして変換できなかったので””を使用
+        textScore.text = ""+score;
+    }
+}
